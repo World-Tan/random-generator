ee6d8fc29a767ddcc45dc21381dba903b68d63d8
MrMagniv <56168212+MrMagniv@users.noreply.github.com>
2023-07-18 17:42:01 +0300

    Add files via upload

---
+++ VisualGen/VisualGen/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace VisualGen
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int[] nums = new int[10];
+			Array.Clear(nums, 0, nums.Length);
+
+			for (long i = 0; i < 100000; i++)
+			{
+				ulong rand = RandGen.MagicRandom();
+				nums[rand % 10]++;
+			}
+
+			// Clear any existing series from the chart (optional)
+			chart1.Series.Clear();
+
+			// Add a new series for the columns
+			Series columnSeries = new Series("ColumnSeries");
+			columnSeries.Color = Color.Turquoise;
+			// Set the chart type to columns (vertical columns)
+			columnSeries.ChartType = SeriesChartType.Column;
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				columnSeries.Points.AddXY(i.ToString(), nums[i]);
+			}
+
+			// Add the series to the chart
+			chart1.Series.Add(columnSeries);
+			
+
+			return;
+		}
+	}
+}
---
+++ VisualGen/VisualGen/RandGen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualGen
+{
+	public static class RandGen
+	{
+		static public ulong Seed = InitSeed();
+
+		static public ulong InitSeed()
+		{
+			ulong seed = (ulong)DateTime.Now.Ticks;
+			seed ^= (ulong)System.Diagnostics.Process.GetCurrentProcess().Id;
+			seed ^= (ulong)Guid.NewGuid().GetHashCode();
+			seed ^= (ulong)Environment.TickCount;
+			seed ^= (ulong)System.Environment.WorkingSet;
+			seed ^= (ulong)System.GC.GetTotalMemory(false);
+			return seed;
+		}
+
+		public static ulong Rol(ulong num, int bits)
+		{
+			return (num << bits) | (num >> (64 - bits));
+
+		}
+		public static ulong Ror(ulong num, int bits)
+		{
+			return (num << (64 - bits)) | (num >> bits);
+
+		}
+
+		static public ulong MagicRandom()
+		{
+			Seed = Ror(Seed, 4 ) + Rol(Seed, 20);
+			Seed = Ror(Seed, 12) + Rol(Seed, 7 );
+			Seed = Ror(Seed, 33) + Rol(Seed, 39);
+			return Seed;
+		}
+	}
+}
